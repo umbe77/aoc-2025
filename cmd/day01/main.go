bc9ca78dd1196cf1a79fba0e6138617e7a787556
Roberto Ughi <umbe77@gmail.com>
2025-12-02 00:01:53 +0100

    refactor

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
@@ -43,26 +44,6 @@ func part1() {
 	fmt.Println("Part 1:", pwd)
 }
 
-func countZeroTouches(start, amount, size int, direction byte) (end int, touches int) {
-
-	if direction == 'R' {
-		// Final position
-		end = (start + amount) % size
-
-		// Number of times we wrap past zero
-		touches = (start + amount) / size
-		return
-	}
-
-	// LEFT rotation:
-	// Final position
-	end = (start - amount%size + size) % size
-
-	// Number of wraps crossing zero
-	touches = max((amount-start+size-1)/size, 0)
-	return
-}
-
 func part2() {
 	movements := make([]int, 0)
 	utils.ReadFile("./cmd/day01/input.txt", func(line string) {
@@ -78,44 +59,29 @@ func part2() {
 		movements = append(movements, steps)
 	})
 
-	const size = 100
-	position := 50
 	totalTouches := 0
+	currentPos := 50
+	for _, move := range movements {
+		if move > 0 {
+			step := currentPos + move
+			currentPos = (step) % 100
+			if step >= 100 {
+				totalTouches += (step / 100)
+			}
 
-	for _, s := range movements {
-		dir := byte('R')
-		if s < 0 {
-			dir = 'L'
+		} else {
+
+			steps := utils.Abs(move)
+			diff := currentPos - steps
+			if diff < 0 {
+				step := math.Ceil(math.Abs(float64(diff)) / 100)
+				totalTouches += int(step)
+			}
+			currentPos = ((diff % 100) + 100) % 100
+
 		}
-		amount := utils.Abs(s)
-		newPos, touches := countZeroTouches(position, amount, size, dir)
-		totalTouches += touches
-		position = newPos
 	}
 
-	// totalTouches := 0
-	// currentPos := 50
-	// for _, move := range movements {
-	// 	if move > 0 {
-	// 		step := currentPos + move
-	// 		currentPos = (step) % 100
-	// 		if step >= 100 {
-	// 			pwd += (step / 100)
-	// 		}
-	//
-	// 	} else {
-	//
-	// 		steps := utils.Abs(move)
-	// 		diff := currentPos - steps
-	// 		if diff < 0 {
-	// 			step := math.Ceil(math.Abs(float64(diff)) / 100)
-	// 			pwd += int(step)
-	// 		}
-	// 		currentPos = ((diff % 100) + 100) % 100
-	//
-	// 	}
-	// }
-	//
 	fmt.Println("Part 2:", totalTouches)
 }
 
