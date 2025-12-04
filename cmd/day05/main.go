b80f643e155825a3da1dfcb84e9c9763b60a9981
Roberto Ughi <umbe77@gmail.com>
2025-12-04 22:43:40 +0100

    removed input files

---
+++ cmd/day05/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/umbe77/aoc-2025/utils"
+)
+
+func part1() {
+	utils.ReadFile("./cmd/day05/sample.txt", func(line string) {
+	})
+
+	// fmt.Println("Part 1:", count)
+}
+
+func part2() {
+	utils.ReadFile("./cmd/day05/sample.txt", func(line string) {
+	})
+
+	// fmt.Println("Part 2", count)
+}
+
+func main() {
+	part1()
+	part2()
+}
