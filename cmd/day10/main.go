aeab3f79ed5880597532b14456af5e8581547200
Roberto Ughi <roberto.ughi@apparound.com>
2025-12-10 19:26:14 +0100

    feat: day10

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"slices"
+	"math"
 	"strings"
 
 	"github.com/umbe77/aoc-2025/utils"
@@ -107,57 +107,6 @@ func bfs(inital, target []int, buttons [][]int) int {
 	return -1 //Impossible
 }
 
-func checkStatesEqual(a, b []int) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func bfs2(initState, target []int, buttons [][]int) int {
-	stateToStr := func(state []int) string {
-		res := ""
-		for _, v := range state {
-			res += fmt.Sprintf("%d,", v)
-		}
-		return res
-	}
-
-	visited := make(map[string]bool)
-	visited[stateToStr(initState)] = true
-	queue := []QueueItem2{{state: initState, presses: 0}}
-
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
-
-		for _, btn := range buttons {
-			newState := slices.Clone(current.state)
-
-			for _, pos := range btn {
-				newState[pos] += 1
-			}
-
-			if checkStatesEqual(newState, target) {
-				return current.presses + 1
-			}
-
-			stateInt := stateToStr(newState)
-			if !visited[stateInt] {
-				visited[stateInt] = true
-				queue = append(queue, QueueItem2{state: newState, presses: current.presses + 1})
-			}
-		}
-	}
-
-	return -1 //Impossible
-}
-
 func part1() {
 	lines := make([]string, 0)
 	utils.ReadFile("./cmd/day10/input.txt", func(line string) {
@@ -176,6 +125,107 @@ func part1() {
 
 }
 
+func solveILP(coeff [][]int, diff []int, numButtons int, maxSinglePress int) int {
+	n := len(diff)
+	bestResult := math.MaxInt32
+
+	var solve func(buttonIndex int, currentPress []int, totalPress int)
+	solve = func(buttonIndex int, currentPress []int, totalPress int) {
+		if totalPress >= bestResult {
+			return
+		}
+		if buttonIndex == numButtons {
+			for i := range n {
+				sum := 0
+				for j := range numButtons {
+					sum += coeff[i][j] * currentPress[j]
+				}
+				if sum != diff[i] {
+					return
+				}
+			}
+			bestResult = totalPress
+			return
+		}
+
+		maxForButton := maxSinglePress
+		for i := range n {
+			if coeff[i][buttonIndex] > 0 {
+				currntSum := 0
+				for j := range buttonIndex {
+					currntSum += coeff[i][j] * currentPress[j]
+				}
+				remaining := diff[i] - currntSum
+				if remaining < 0 {
+					return
+				}
+				if remaining < maxForButton {
+					maxForButton = remaining
+				}
+			}
+		}
+
+		for presses := 0; presses <= maxForButton; presses++ {
+			currentPress[buttonIndex] = presses
+			solve(buttonIndex+1, currentPress, totalPress+presses)
+		}
+	}
+
+	currentPress := make([]int, numButtons)
+	solve(0, currentPress, 0)
+
+	if bestResult == math.MaxInt32 {
+		return -1
+	}
+	return bestResult
+}
+
+func minButtonPresses(initial, target []int, buttons [][]int) int {
+	n := len(initial)
+	numButtons := len(buttons)
+
+	diff := make([]int, n)
+	for i := range n {
+		diff[i] = target[i] - initial[i]
+		if diff[i] < 0 {
+			return -1
+		}
+	}
+
+	allZero := true
+	for _, d := range diff {
+		if d != 0 {
+			allZero = false
+			break
+		}
+	}
+	if allZero {
+		return 0
+	}
+
+	coeff := make([][]int, n)
+	for i := range n {
+		coeff[i] = make([]int, numButtons)
+	}
+	for j, btn := range buttons {
+		for _, pos := range btn {
+			if pos < n {
+				coeff[pos][j] = 1
+			}
+		}
+	}
+
+	maxPresses := 0
+	for _, d := range diff {
+		if d > maxPresses {
+			maxPresses = d
+		}
+	}
+
+	result := solveILP(coeff, diff, numButtons, maxPresses)
+	return result
+}
+
 func part2() {
 	lines := make([]string, 0)
 	utils.ReadFile("./cmd/day10/input.txt", func(line string) {
@@ -186,9 +236,8 @@ func part2() {
 	for _, line := range lines {
 		_, buttons, jolts := parseInput(line)
 		initState := make([]int, len(jolts))
-		presses := bfs2(initState, jolts, buttons)
+		presses := minButtonPresses(initState, jolts, buttons)
 		sum += presses
-		break
 	}
 
 	fmt.Println("Part 2:", sum)
